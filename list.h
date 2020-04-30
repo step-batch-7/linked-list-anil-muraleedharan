90dcce9da017821961ab03c92a0e086935ce6dbe
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-05-01 00:54:35 +0530

    declared is_number_exist fuction & defined message

--- list.h
+++ list.h
@@ -24,6 +24,8 @@ typedef struct
 
 typedef List *List_ptr;
 
+#define Message "\n```\nMain Menu\n---------\n(a) add a number to the end of the list\n(b) add a number to the start of the list\n(c) insert a number at a given position in the list\n(d) add a unique item on the list at the end(if it already exists, do not insert)\n(e) remove a number from the beginning of the list\n(f) remove a number from the end of the list\n(g) remove a number from a given position in the list\n(h) remove first occurrence of a number\n(i) remove all occurrences of a number\n(j) clear the whole list\n(k) check if a number exists in the list\n(l) display the list of numbers\n(m) exit\n\nPlease enter the alphabet of the operation you would like to perform\n```\n"
+
 List_ptr create_list(void);
 
 Status add_to_end(List_ptr, int value);
@@ -39,6 +41,7 @@ Status remove_at(List_ptr, int position);
 
 Status remove_first_occurrence(List_ptr, int value);
 Status remove_all_occurrences(List_ptr, int value);
+Status is_number_exist(List_ptr, int value);
 
 Status clear_list(List_ptr); // Removes all elements in the list
 
